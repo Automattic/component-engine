46771e917d48a149877ef2bb33712dc35971294d
Payton Swick <payton@foolord.com>
2016-12-15 16:43:18 -0500

    Pass componentId as a prop to the component

--- src/lib/component-builder.js
+++ src/lib/component-builder.js
@@ -14,27 +14,17 @@ function buildComponent( Component, props = {}, children = [] ) {
 	return <Component key={ props.key } { ...props }>{ children }</Component>;
 }
 
-function buildComponentTreeFromConfig( componentConfig ) {
+function buildComponentFromConfig( componentConfig ) {
 	const { id, componentType, children, props } = componentConfig;
 	const componentId = id || shortid.generate();
 	const Component = getComponentByType( componentType );
-	const childComponents = children ? children.map( child => buildComponentTreeFromConfig( child ) ) : null;
+	const childComponents = children ? children.map( child => buildComponentFromConfig( child ) ) : null;
 	const componentProps = Object.assign(
 		{},
 		props || {},
-		{ className: classNames( componentType, componentId ), key: componentId }
+		{ className: classNames( componentType, componentId ), key: componentId, componentId }
 	);
-	return { Component, componentId, componentProps, childComponents, componentType };
-}
-
-function buildComponentFromTree( tree ) {
-	const { Component, componentProps, childComponents } = tree;
-	const children = childComponents ? childComponents.map( child => buildComponentFromTree( child ) ) : null;
-	return buildComponent( Component, componentProps, children );
-}
-
-function buildComponentFromConfig( componentConfig ) {
-	return buildComponentFromTree( buildComponentTreeFromConfig( componentConfig ) );
+	return buildComponent( Component, componentProps, childComponents );
 }
 
 export function renderComponent( componentConfig ) {
