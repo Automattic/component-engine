ed2524bbf6c6d1ae363f9e4de25995cd6c8bede2
Payton Swick <payton@foolord.com>
2016-12-14 22:59:33 -0500

    Fix import path in core-components

--- src/components/core-components.js
+++ src/components/core-components.js
@@ -1,18 +1,18 @@
 /**
  * Internal dependencies
  */
-import '~/src/themes/components/HeaderText';
-import '~/src/themes/components/PostList';
-import '~/src/themes/components/TextWidget';
-import '~/src/themes/components/MenuWidget';
-import '~/src/themes/components/SearchWidget';
-import '~/src/themes/components/FooterText';
-import '~/src/themes/components/PostBody';
-import '~/src/themes/components/PostTitle';
-import '~/src/themes/components/PostDate';
-import '~/src/themes/components/PostAuthor';
-import '~/src/themes/components/PostContent';
-import '~/src/themes/components/ColumnComponent';
-import '~/src/themes/components/PageLayout';
-import '~/src/themes/components/RowComponent';
-import '~/src/themes/components/SinglePost';
+import '~/src/components/HeaderText';
+import '~/src/components/PostList';
+import '~/src/components/TextWidget';
+import '~/src/components/MenuWidget';
+import '~/src/components/SearchWidget';
+import '~/src/components/FooterText';
+import '~/src/components/PostBody';
+import '~/src/components/PostTitle';
+import '~/src/components/PostDate';
+import '~/src/components/PostAuthor';
+import '~/src/components/PostContent';
+import '~/src/components/ColumnComponent';
+import '~/src/components/PageLayout';
+import '~/src/components/RowComponent';
+import '~/src/components/SinglePost';
