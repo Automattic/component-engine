f2489e4cb017e2dd30011ab363b58c452c63e137
Payton Swick <payton@foolord.com>
2016-12-19 18:28:51 -0500

    Wrap components in spans with data when rendering

--- src/lib/component-builder.js
+++ src/lib/component-builder.js
@@ -5,13 +5,34 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import classNames from 'classnames';
 import shortid from 'shortid';
+import omit from 'lodash/omit';
+import includes from 'lodash/includes';
 
 /**
  * Internal dependencies
  */
 import { getComponentByType } from '~/src/lib/components';
 
+const Wrapper = ( props ) => {
+	const { children, className } = props;
+	const childProps = omit( props, [ 'children', 'className', 'data-block-type', 'data-block-id' ] );
+	const newChildren = React.Children.map( children, child => React.cloneElement( child, { ...childProps } ) );
+	const dataKeys = [ 'data-block-type', 'data-block-id' ];
+	const dataProps = Object.keys( props ).filter( key => includes( dataKeys, key ) ).reduce( ( newProps, key ) => {
+		newProps[ key ] = props[ key ];
+		return newProps;
+	}, {} );
+	return (
+		<span className={ className } { ...dataProps }>
+			{ newChildren }
+		</span>
+	);
+};
+
 function buildComponent( Component, props = {}, children = [] ) {
+	if ( props.renderingToString ) {
+		return <Wrapper key={ 'Wrapper-' + props.componentId } data-block-type={ props.componentType } data-block-id={ props.componentId }><Component key={ props.key } { ...props }>{ children }</Component></Wrapper>;
+	}
 	return <Component key={ props.key } { ...props }>{ children }</Component>;
 }
 
@@ -24,7 +45,7 @@ function buildComponentFromConfig( componentConfig, additionalProps = {} ) {
 	const componentProps = Object.assign(
 		{},
 		props || {},
-		{ className: classNames( componentType, componentId ), key: componentId, componentId, renderingToString }
+		{ className: classNames( componentType, componentId ), key: componentId, componentType, componentId, renderingToString }
 	);
 	return buildComponent( Component, componentProps, childComponents );
 }
